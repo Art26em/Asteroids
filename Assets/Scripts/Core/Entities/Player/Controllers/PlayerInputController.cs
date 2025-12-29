14dc5b6820931d848972d33ee561e5072730701f
Artem Timonin <t1moninartyom@yandex.ru>
2025-12-29 20:30:40 +0500

    PlayerInputController.cs

---
+++ Assets/Scripts/Core/Entities/Player/Controllers/PlayerInputController.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+namespace Core.Entities.Player.Controllers
+{
+    public class PlayerInputController : MonoBehaviour
+    {
+        private void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.W))
+            {
+                // DO smth
+            }
+        }
+    }
+}
