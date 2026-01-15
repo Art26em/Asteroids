040607a7de9371db70f4b50d568bc98c3580dcbe
Artem Timonin <t1moninartyom@yandex.ru>
2026-01-15 20:36:45 +0500

    Начата работа над телепортом игрока

--- Assets/Scripts/Core/Entities/Player/Controllers/PlayerInputController.cs
+++ Assets/Scripts/Core/Entities/Player/Controllers/PlayerInputController.cs
@@ -1,3 +1,4 @@
+using System;
 using Core.Entities.Player.Movement;
 using Core.SpriteControllers;
 using UnityEngine;
@@ -9,6 +10,7 @@ namespace Core.Entities.Player.Controllers
     {
     	private PlayerMover _playerMover;
         private PlayerSpriteController _playerSpriteController;
+        private Camera _camera;
 
         [Inject]
         private void Construct(PlayerMover playerMover, PlayerSpriteController playerSpriteController)
@@ -16,13 +18,45 @@ namespace Core.Entities.Player.Controllers
             _playerMover = playerMover;
             _playerSpriteController = playerSpriteController;
         }
-        
-		private void Update()
+
+        private void Awake()
+        {
+            _camera = Camera.main;
+        }
+
+        private void Update()
         {
             _playerMover?.Move(GetMovementInput(), Time.deltaTime); 
             _playerSpriteController?.UpdatePlayerSprite();
+			CheckPlayerWorldPosition();
         }
-        
+
+        private void CheckPlayerWorldPosition()
+        {
+           var viewportPosition = _camera.WorldToViewportPoint(transform.position);
+           if (viewportPosition.x < 0)
+           {
+               viewportPosition.x = 1;
+               transform.position = _camera.ViewportToWorldPoint(viewportPosition);
+           }
+           // if (viewportPosition.x > 1)
+           // {
+           //     viewportPosition.x = 0;
+           //     transform.position = _camera.ViewportToWorldPoint(viewportPosition);
+           // }
+           if (viewportPosition.y < 0)
+           {
+               viewportPosition.y = 1;
+               transform.position = _camera.ViewportToWorldPoint(viewportPosition);
+           } 
+           if (viewportPosition.y > 1)
+           {
+               viewportPosition.y = 0;
+               transform.position = _camera.ViewportToWorldPoint(viewportPosition);
+           }
+           Debug.Log(viewportPosition);
+        }
+
         private Vector2 GetMovementInput()
         {
             return new Vector2(Input.GetAxis(AxisNames.Horizontal), Input.GetAxis(AxisNames.Vertical));
